8bff7782be9ab353e087c3110dacf8c63548a40b
sklorde <53619830+sklorde@users.noreply.github.com>
2019-11-19 15:27:23 -0300

    TESTE

---
+++ classes/instalador.pas
@@ -0,0 +1,51 @@
+unit instalador;
+
+interface
+
+type
+  TInstalador = class
+
+  private
+    procedure criaBat;
+  public
+    procedure configurarHD;
+    procedure configurarDB;
+    procedure moverArquivos;
+    procedure instalarProgramas;
+    procedure criarAtalhos;
+  end;
+
+implementation
+
+{ TInstalador }
+
+procedure TInstalador.configurarDB;
+begin
+
+end;
+
+procedure TInstalador.configurarHD;
+begin
+
+end;
+
+procedure TInstalador.criaBat;
+begin
+
+end;
+
+procedure TInstalador.criarAtalhos;
+begin
+
+end;
+
+procedure TInstalador.instalarProgramas;
+begin
+
+end;
+
+procedure TInstalador.moverArquivos;
+begin
+
+end;
+end.
